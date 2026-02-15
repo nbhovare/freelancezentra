71d4153f07c35edc9c3f873c5a118cf7a9270711
nbhovare <nbhovare@gmail.com>
2026-02-15 22:47:11 +0530

    config added env sub_id

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,7 +19,7 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id = "${{ secrets.SUB_ID }}"
-  resource_provider_registrations = "none"
+  subscription_id = "$sub_id"
+  resource_provider_registrations = "none"  
 
 }
